fb12e0cad32724668309ab779a2a5c2fd9797bfd
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-10 11:40:38 +0500

    dastur kodi yuklandi

---
+++ amallar.py
@@ -0,0 +1,19 @@
+a,b  = map(int,input().split())
+# add
+addition = a+b
+#sub
+subtraction = a-b
+#div
+division = a/b
+#fdiv
+floor_division = a//b
+#mul
+multiplication = a*b
+
+
+#natijani chop etish
+print(addition) 
+print(subtraction)
+print(division)
+print(floor_division)
+print(multiplication)
